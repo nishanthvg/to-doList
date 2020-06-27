666f32e865f8c3c1813882d2d7656f0c1c515f3d
nishanthvg <60750619+nishanthvg@users.noreply.github.com>
2020-06-27 12:02:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+let port = 3000;
+let itemArray = [];
+
+app.get("/", (req,res) => {
+
+    let options = {
+        weekday: "long",
+        month: "long",
+        day: "numeric"
+    }
+    let today = new Date ();
+    let day = today.toLocaleDateString("en-US",options);
+    //Sends the day to ejs file
+    res.render('list', {kindOfDay: day, newListItem: itemArray });
+
+}); 
+
+app.post("/",(req,res) => {
+    let item = req.body.newItem;
+    itemArray.push(item);
+    res.redirect("/");
+})
+
+app.listen(port, () => {
+    console.log("Server is running in port " + port);
+});
